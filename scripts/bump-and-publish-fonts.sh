a5a53962d350b9dbb0c5034e4ed1ef30c03ad766
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2025-04-24 20:18:53 -0400

    Move update out of loop
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- scripts/bump-and-publish-fonts.sh
+++ scripts/bump-and-publish-fonts.sh
@@ -4,12 +4,12 @@ SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 REPO_DIR=$(realpath ${SCRIPT_DIR}/..)
 pushd ${REPO_DIR}
 
+update-moonbit-version-readme
 update-moonbit-deps
 for i in ../mbt-fonts-* ; do
     echo $i && pushd $i
     VERSION=$(grep version moon.mod.json | sed -e 's/^.* "//g' -e 's/".*$//')
     ./update.sh
-    update-moonbit-version-readme
     git commit -sam "Bump version to ${VERSION}"
     git push
     # Remove JSON font representations before publishing, then restore.
