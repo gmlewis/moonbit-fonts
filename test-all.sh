1cce4bc0c8b204f43eedb05b0a531c5dd5825f8c
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2026-01-12 22:23:58 -0500

    Fix tests
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- test-all.sh
+++ test-all.sh
@@ -3,4 +3,6 @@ moon fmt && moon info
 moon test --target all
 
 # Ensure that all fonts load without error:
-# moon run --target native tests/load-all-fonts
+# export MOONBIT_FONTS_DIR="$(dirname "$(readlink -f "$0")")"
+# pushd tests/load-all-fonts && moon run . && popd
+
